2b006c6881802c908260e81cc109a0e0569e2ced
merttas1994 <mert_gs.1234@hotmail.com>
2020-02-12 19:22:21 +0300

    Ingredient commit

--- coffee_machine_automaton/src/coffee_machine_automaton/CoffeeMachine.java
+++ coffee_machine_automaton/src/coffee_machine_automaton/CoffeeMachine.java
@@ -7,7 +7,34 @@ package coffee_machine_automaton;
  * @author merttas
  *
  */
+
+import java.util.Scanner;
+
 public class CoffeeMachine {
+static Scanner input=new Scanner(System.in);
+	
+	private double coffee_powder, milk, water;
+    private int count_coffee = 0;
+    
+    public CoffeeMachine(){ // Initial values to zero...
+        this.coffee_powder=0;
+        this.milk=0;
+        this.water=0;
+    }
+    
+    private void setIngredient(){   // Fill Ingredient to Machine
+        System.out.println("\nIt is filling to machine now...");
+        this.coffee_powder = 1000.0;
+        this.milk = 10;
+        this.water= 10;
+        System.out.println("Filling Completed.");
+    } 
+    
+    private void getIngredient(){   // To Get Status
+        System.out.println("Available Coffee Powder(Gr) "+String.format("%.1f",this.coffee_powder));
+        System.out.println("Available Milk(Lt) "+String.format("%.1f", this.milk));
+        System.out.println("Available Water(Lt) "+String.format("%.1f", this.water));
+}
 
 	/**
 	 * @param args
